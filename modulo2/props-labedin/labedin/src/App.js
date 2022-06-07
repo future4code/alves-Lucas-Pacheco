d6f539dda785b151d3ab981cab4710271605b623
Lucas Magalhães Pacheco <lucasmagalhaespacheco@gmail.com>
2022-06-07 15:34:55 -0300

    Alterações foto, texto inicial, setinha

---
+++ modulo2/props-labedin/labedin/src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './App.css';
+import CardGrande from './components/CardGrande/CardGrande';
+import ImagemButton from './components/ImagemButton/ImagemButton';
+import MinhaFoto from "./img/Lucas.jpg"
+import seta from "./img/seta.png"
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page-section-container">
+        <h2>Dados pessoais</h2>
+        <CardGrande 
+          imagem= {MinhaFoto} 
+          nome="Lucas  Magalhães Pacheco" 
+          descricao="Oi, eu sou Lucas, sou um amante jogador de RPG e um odiador de CSS com todas minhas forças, devido a isso estou aqui para fazer minha vingança contra o CSS! ME ajudem nessa jornada em busca pelo caos."
+        />
+        
+        <ImagemButton 
+          imagem={seta} 
+          texto="Ver mais"
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Experiências profissionais</h2>
+        <CardGrande 
+          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
+          nome="Labenu" 
+          descricao="Formando desenvolvedores para o mercado de trabalho!" 
+        />
+        
+        <CardGrande 
+          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
+          nome="NASA" 
+          descricao="Apontando defeitos." 
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Minhas redes sociais</h2>
+        <ImagemButton 
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
+          texto="Facebook" 
+        />        
+
+        <ImagemButton 
+          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
+          texto="Twitter" 
+        />        
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ modulo2/props-labedin/labedin/src/components/ImagemButton/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const  ImagemButton = styled.ImagemButton `
+    display: flex;
+    align-items: center;
+    border: 1px solid black;
+    border-radius: 50px;
+    width: 200px;
+    padding: 15px 30px;
+    `

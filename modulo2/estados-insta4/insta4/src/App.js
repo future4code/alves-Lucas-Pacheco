cf978893ac2e2803170b9cac0aa2cc7f848e6667
Lucas Magalhães Pacheco <lucasmagalhaespacheco@gmail.com>
2022-06-09 17:39:09 -0300

    Alterações finais

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -1,3 +1,4 @@
+import { findByLabelText } from '@testing-library/react';
 import React from 'react';
 import styled from 'styled-components'
 import Post from './components/Post/Post';
@@ -8,7 +9,13 @@ const MainContainer = styled.div`
   flex-direction: column;
   align-items: center;
 `
+const Formulario = styled.section`
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ margin-bottom: 3%;
 
+`
 class App extends React.Component {
   state = {
    pessoa: [
@@ -32,25 +39,90 @@ class App extends React.Component {
    
    
    
-]
+    ],
+
+    valorInputNome: "",
+    valorInputFotoUsuario: "",
+    valorInputFotoPost: ""
 
 
 }
 
+adicionaPost = () => {
+
+  const preparacaoDoPost = {
+    nomeUsuario: this.state.valorInputNome,
+
+    fotoUsuario: this.state.valorInputFotoUsuario,
+
+    fotoPost: this.state.valorInputFotoPost
+  }
+
+  const novoPost = [...this.state.pessoa, preparacaoDoPost]
+   
+  this.setState({
+    pessoa: novoPost,
+    valorInputNome: "",
+    valorInputFotoUsuario: "",
+    valorInputFotoPost: ""
+  })
+
+}
+  onChangeInputNome = (event) => {
+    this.setState({
+      valorInputNome: event.target.value
+    })
+  }
+
+  onChangeInputFotoUsuario = (event) => {
+    this.setState({
+      valorInputFotoUsuario: event.target.value
+    })
+  }
+
+  onChangeInputFotoPost = (event) => {
+    this.setState({
+      valorInputFotoPost: event.target.value
+    })
+  }
+
+
+
   render() {
-    const listaDePost = this.state.pessoa.map((post) => {
-      return <Post
+    const listaDePost = this.state.pessoa.map((post, indice) => {
+      return <Post key={indice}
         nomeUsuario={post.nomeUsuario}
         fotoUsuario={post.fotoUsuario}
-        fotoPost={post.fotoPost}/> 
-
-
-      
-       
+        fotoPost={post.fotoPost}/>   
       
     })
     return (
       <MainContainer>
+
+        <h1> Criação de Post</h1>
+       
+       <Formulario>
+         
+
+         <input
+         value= {this.state.valorInputNome}
+         onChange={this.onChangeInputNome}
+         placeholder={"Nome Usuario"} />
+
+        <input
+         value= {this.state.valorInputFotoUsuario}
+         onChange={this.onChangeInputFotoUsuario}
+         placeholder={"Link Foto Usuário"} />
+
+        <input
+         value= {this.state.valorInputFotoPost}
+         onChange={this.onChangeInputFotoPost}
+         placeholder={"Link Foto do Post"} />
+
+         <button onClick={this.adicionaPost}>Adicionar</button>
+                  
+        </Formulario>
+             
         {listaDePost}
 
       </MainContainer>

8c430bb5f80602991627c538155dbbe019a23e77
Lucas Magalhães Pacheco <lucasmagalhaespacheco@gmail.com>
2022-06-12 11:28:40 -0300

    Só arrumando o Styled

--- modulo2/projeto-whatslab/whatslab/src/App.js
+++ modulo2/projeto-whatslab/whatslab/src/App.js
@@ -1,63 +1,8 @@
 
 import './App.css';
 import React from 'react';
-import styled from 'styled-components'
+import {AppWhats, TerminalTotal, WhatLabTerminal, DisplayDePerguntas, CaixaDeMensagem, NomeUsuario, CaixaDeMensagemTotal} from "./components/sytled"
 
-const AppWhats = styled.section `
-    display:flex;
-    justify-content: center;`
-
-const TerminalTotal = styled.div `
-     max-width: 600px;
-     height: 100vh;
-     border:1px solid black;
-     flex: 1 1 0%;
-     display: flex;
-     flex-direction: column-reverse;
-     background-color: gray;`
-   
-
-const WhatLabTerminal = styled.section `
-    flex: 1 1 0%;
-    display: flex;
-    flex-direction: column;
-    -webkit-box-pack: end;
-    justify-content: flex-end;
-    padding: 20px;
-    border: 2px solid black`
-
-
-const DisplayDePerguntas = styled.section `
-  display: grid;
-  grid-template-columns: 120px 1fr 80px;
-  gap: 12px;
-  height: 40px;
-  padding: 14px;
-  border-radius: 50px;
-  border: 1px black solid;
-`
-
-const CaixaDeMensagem = styled.div `
-    display: flex;
-    flex-direction: column;
-    margin: 10px 0px;
-    border-radius: 5px;
-
-    
-`
-
-const NomeUsuario = styled.h4 `
-     margin-bottom: 10px;
-    font-weight: bold;`
-
-const CaixaDeMensagemTotal = styled.div `
-    max-width: 40%;
-    height: 100%;
-    text-align: left;
-    align-self: flex-start;
-    display: flex;
-    flex-direction: column;
-`
   
 
 class App extends React.Component {

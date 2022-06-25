b73790267c7933b59b6eda7eb0507f9b3bf452f5
Lucas Magalhães Pacheco <lucasmagalhaespacheco@gmail.com>
2022-06-25 15:40:39 -0300

    Estilizações Página Add-Musicas

--- modulo2/projeto-labefy/labefy/src/Paginas/detalhesMusicas.jsx
+++ modulo2/projeto-labefy/labefy/src/Paginas/detalhesMusicas.jsx
@@ -1,5 +1,142 @@
 import axios from 'axios'
 import React, { Component } from 'react'
+import styled from 'styled-components'
+import {GiDaggerRose, GiEvilFork, GiExtraTime, GiFluffyTrefoil} from "react-icons/gi"
+
+ const ContainerPrincipal = styled.div `
+ display: flex;
+ flex-direction: Column;
+ justify-content: center;
+ align-items: center;
+ background-color: lightcyan;
+ 
+ `
+
+const  ContainerSongs = styled.div `
+ display: flex;
+ flex-direction: column;
+ justify-content: center;
+ align-items: center;
+ text-align: center;
+ border: 2px orange solid;
+ background-color: lightcyan;
+ border-radius: 1rem;
+ margin-bottom: 0.5%;
+
+ h1 {
+    color: pink;
+    text-align: center;
+    font-size: 1.4rem;
+ }
+
+ h4 {
+    color: purple;
+    text-align: center;
+    font-size: 1.3rem;
+ }
+
+ button {
+    margin-right: -3%;
+    background-color: lightblue;
+    font-size: 1rem;
+    text-align: center;
+    align-items: center;
+    margin-top: 3%;
+    width: 20%;
+    font-size: 1.3rem;
+    border-radius: 1rem;
+
+ }
+ button:hover {
+    background-color: purple;
+    color: red;
+ }
+ button:active {
+    background: lightyellow;
+    color: darkblue;
+ }
+ audio {
+    align-items: center;
+ }
+`
+
+
+const  ContainerInicial = styled.div `
+color: pink;
+text-align:center;
+
+button {
+    background-color: lightblue;
+    border: 1px solid black;
+    border-radius: 1rem;
+    font-size: 1rem;
+
+}
+
+button:hover {
+    background-color: pink;
+    color: red;
+ }
+
+p {
+     color: purple;
+     font-size: 1.2rem;
+}`
+
+const ContainerAdicionarMusica = styled.div `
+background-color: lightcyan;
+ display: flex;
+ flex-direction: column;
+ justify-content: center;
+ font-size: 2rem;
+ margin-bottom: 2%;
+
+ h1 {
+    text-align: center;
+    color: pink;
+ }
+
+ input {
+ background-color: lightcyan;
+ margin-bottom: 3%;
+ font-size: 1rem;
+
+}
+button {
+    margin-right: 2%;
+    background-color: lightblue;
+    font-size: 1.5rem;
+    text-align: center;
+    align-items: center;
+
+ }
+ button:hover {
+    background-color: purple;
+    color: red;
+ }
+ button:active {
+    background: lightyellow;
+    color: darkblue;
+ }`
+ 
+ const InformationAdd = styled.div `
+  text-align:center;
+  border : 1px solid orange;
+  border-radius: 1rem;
+  h3 {
+    color: pink;
+    font-size: 1.5rem;
+  }
+  
+  p {
+    color: purple;
+    font-size: 1.2rem;
+  }`
+
+const DivLarge = styled.div `
+background-color: lightcyan;
+height: 100%;
+`
 
 export default class detalhesMusicas extends Component {
     state = {
@@ -37,8 +174,6 @@ export default class detalhesMusicas extends Component {
           })
       }
       
-
-   
 
     onClickAdicionarMusica = (id) => {
         const novaMusica = {
@@ -99,26 +234,28 @@ export default class detalhesMusicas extends Component {
         
     }
 
-
-
       render() {
        const  listaDeMusicasAtualizada = this.state.listaDeMusicas.map((musica) => {
-        return <div key={musica.id}>
-            <li>{musica.name}</li>
-            <li>{musica.artist}</li>
-             <li>{musica.url}</li>
+        return <ContainerSongs key={musica.id}>
+            <h1>{musica.name} <GiExtraTime /> </h1>
+            <h4>Nome do Artista: {musica.artist} <GiEvilFork /> </h4>
             <audio ref="audio_tag" src={musica.url} controls />
-            <button onClick={() => this.onClickApagarMusica(musica.id)}>Apagar Musica</button>
+            <button onClick={() => this.onClickApagarMusica(musica.id)}>Apagar Musica <GiDaggerRose /></button>
 
-        </div>
+        </ContainerSongs>
 
        })
 
-    
-
         return (
-            <div>
-                {listaDeMusicasAtualizada}
+            <DivLarge>
+            <ContainerPrincipal>  
+                <ContainerInicial>
+                <h1> Retorne para Sua Playlist Aqui: </h1>
+                <p>Voltar a sua Playlists clicando abaixo: </p>
+                <button onClick={() => this.props.irParaPlaylist()}> Veja suas Playlists</button>
+                </ContainerInicial>
+                <ContainerAdicionarMusica>
+                <h1> Suas Músicas da Playlist: </h1>
                 <input
                value={this.state.valorInputAdicionarMusica}
                placeholder="Insira nome da Sua Musica"
@@ -127,18 +264,22 @@ export default class detalhesMusicas extends Component {
                value={this.state.valorInputAdicionarArtista}
                placeholder="Insira o nome do seu artista"
                onChange={this.onChangeValorInputAdicionarArtista} />
-
                <input
                value={this.state.valorInputAdicionarUrl}
                placeholder="Insira URL da sua musica"
                onChange={this.onChangeValorInputAdicionarUrl} />
-
-              <button onClick={() => this.onClickAdicionarMusica(this.props.id)}>Enviar sua Playlist</button>
+              <button onClick={() => this.onClickAdicionarMusica(this.props.id)}>Enviar sua Musica < GiFluffyTrefoil /></button>
+              </ContainerAdicionarMusica>
+              <hr />
+              <InformationAdd>
+              <h3> Informações Adicionais</h3>
+              <a href="http://spoti4.future4.com.br/5.mp3">Musicas 1 a 100</a>
+              <p> Neste Link em questão temos algumas músicas que vocês podem colocar, modifiquem o número 5 para entre 1 a 99 e poderá adicionar músicas diferentes, nosso site está no beta no futuro terão mais músicas a adicionar </p>
+              </InformationAdd> 
                 <hr />
-
-                <button onClick={() => this.props.irParaPlaylist()}> Veja suas Playlists</button>
-
-            </div>
+            </ContainerPrincipal>
+                {listaDeMusicasAtualizada}
+            </DivLarge>
         )
     }
 }

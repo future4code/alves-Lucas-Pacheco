b05167ccc7f56bc8ae55e54fe27e0d20b5602f58
Lucas Magalhães Pacheco <lucasmagalhaespacheco@gmail.com>
2022-07-06 16:38:46 -0300

    mudando o URL

--- modulo3/projeto-astromatch/astromatch/src/constants/credentials.js
+++ modulo3/projeto-astromatch/astromatch/src/constants/credentials.js
@@ -1,7 +1,7 @@
-export const URL_PERSON = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/lucas/person"
+export const URL_PERSON = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/lucasmagalhaes/person"
 
-export const URL_MATCHES = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/lucas/matches"
+export const URL_MATCHES = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/lucasmagalhaes/matches"
 
-export const URL_CHOOSE = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/lucas/choose-person"
+export const URL_CHOOSE = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/lucasmagalhaes/choose-person"
 
-export const URL_CLEAR = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/lucas/clear"
+export const URL_CLEAR = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/lucasmagalhaes/clear"

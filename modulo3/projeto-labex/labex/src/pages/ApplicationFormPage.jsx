7c00d3e695620e348f019826a9883b9c2e8456e4
Lucas Magalhães Pacheco <lucasmagalhaespacheco@gmail.com>
2022-07-14 18:18:36 -0300

    Arrumar Bugs amanhã

--- modulo3/projeto-labex/labex/src/pages/ApplicationFormPage.jsx
+++ modulo3/projeto-labex/labex/src/pages/ApplicationFormPage.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from '../components/Header'
 import axios from 'axios'
-import { BASE_URL, HEADERS } from '../constants/credentiais'
+import { BASE_URL } from '../constants/credentiais'
 import { useForm } from '../hooks/useForm'
-import { goToListTripsPage } from '../routes/cordinator'
+import { goToPreviousPage } from '../routes/cordinator'
 import { useNavigate } from 'react-router-dom'
 import { useGetData } from '../hooks/useGetData'
+import { Countries } from '../components/Countries'
+
+
 export default function ApplicationFormPage() {
   const navigate = useNavigate()
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState()
   const {form, handleChange, cleanFields} = useForm({
     name: "",
     age: "",
@@ -17,14 +20,92 @@ export default function ApplicationFormPage() {
     country: "",
     tripId: ""
   })
-  const [dados, loading, erro] = useGetData("/trips")
+
+  const {dados} = useGetData("/trips/")
+  const trips = dados?.trips
   
-  const onSubmitCreate = () => {
-    
+  const onSubmitCreate = (event) => {
+    event.preventDefault()
+    setLoading(true)
+    const  body = {
+      name: form.name,
+      age: form.age,
+      applicationText: form.applicationText,
+      profession: form.profession,
+      country: form.country
+    }
+    axios
+    .post(`${BASE_URL}/trips/${form.tripId}/apply`, body)
+    .then((res) => {
+      alert(`Ìnscrição realizada com sucesso!`)
+      cleanFields()
+      setLoading(false)
+    })
+    .catch((err) => {
+      alert(err.response.message)
+    })
   }
+
+
   return (
     <div>
       < Header />
-      <h1>ApplicationFormPage</h1></div>
+      <h1>Se Cadastre para uma Viagem</h1>
+      <button onClick={() => goToPreviousPage(navigate)}>Voltar</button>
+      <form onSubmit={onSubmitCreate}>
+        <select name={"tripId"} 
+        defaultValue={""} 
+        onChange={handleChange}
+        required>
+          <option value="" disabled>Escolha Uma Aventura</option>
+          {dados && trips.map((trip) => {
+            return (
+              <option key={trip.id} value={trip.id}>{trip.name}</option>
+            )
+          })}
+        </select>
+      <input 
+      name="name" 
+      type="text" 
+      value={form.name} 
+      onChange={handleChange}
+      placeholder="Seu Nome"
+      pattern={"^.{3,}"}
+      title={"Seu nome deve ter no mínimo 3 letras"}
+      required
+      />
+      <input 
+      name='age'
+      type='number'
+      value={form.age}
+      onChange={handleChange}
+      placeholder='Idade'
+      min={16}
+      required
+      />
+      <input 
+      name='applicationText'
+      value={form.applicationText}
+      onChange={handleChange}
+      placeholder='Por que você gostaria de viajar conosco?'
+      pattern={"^.{10,}"}
+      title={"Vamos escreva mais um pouco!"}
+      required
+      />
+      <input 
+      name='profession'
+      value={form.profession}
+      onChange={handleChange}
+      placeholder='Sua profissão'
+      pattern={"^.{2,}"}
+      title="Sua profissão precisa ter mais que 2 caracteres"
+      />
+      <select name={"country"} defaultValue={""} onChange={handleChange} required>
+          <option value="" disabled>Escolha um País</option>
+          < Countries /> 
+        </select>
+        <button>Enviar sua inscrição!</button>
+      </form>
+    </div>
   )
 }
--- modulo3/projeto-labex/labex/src/pages/TripDetailsPage.jsx
+++ modulo3/projeto-labex/labex/src/pages/TripDetailsPage.jsx
@@ -12,9 +12,8 @@ export default function TripDetailsPage() {
   const [loading, setLoading] = useState(false)
   const [erro, setErro] = useState()
   const [tripInfo, setTripInfo] = useState()
+  const [candidates, setCandidates] = useState([])
 
-  // const { dados, loading, erro } = useGetData(`/trip/${params.id}`)
-  // const tripsInfo = dados?.trip
   
   
   useEffect(() => {
@@ -25,6 +24,7 @@ export default function TripDetailsPage() {
     console.log(res)
     setLoading(false)
     setTripInfo(res.data.trip)
+    setCandidates(res.data.trip.candidates)
    })
    .catch((err) => {
     setLoading(false)
@@ -33,17 +33,67 @@ export default function TripDetailsPage() {
    })
   }, [])
 
+  const decisionCadidates = (candidatesId, choice) => {
+    const body = {
+      approve: choice
+    }
+
+    axios.put(`${BASE_URL}/trips/${tripInfo.id}/candidates/${candidatesId}/decide`, body, HEADERS)
+    .then((res) => {
+      if(choice === true) {
+        alert("Candidato aprovado!")
+
+      } else {
+        alert("O Canditato foi reprovado")
+      }
+      document.location.reload(true)
+    })
+    .catch((err) => {
+      alert("Um Erro aconteceu entre em contato com grupo de devs")
+    })
+  }
+  const pendingCanditates = candidates.map((candidate) => {
+    <div key={candidate.id}>
+      <p>{candidate.name}</p>
+      <p>{candidate.age}</p>
+      <p>{candidate.profession}</p>
+      <p>{candidate.country}</p>
+      <p>{candidate.applicationText}</p>
+      <button onClick={() => decisionCadidates(candidate.id, true)}>Aprovar</button>
+      <button onClick={() => decisionCadidates(candidate.id, false)}>Reprovar</button>
+    </div>
+  })
+  const aprovedCandidates = tripInfo && tripInfo.approved.map((candidate) => {
+    <div key={candidate.id}>
+    <p>{candidate.name}</p>
+    <p>{candidate.age}</p>
+    <p>{candidate.profession}</p>
+    <p>{candidate.country}</p>
+    <p>{candidate.applicationText}</p>
+  </div>
+  })
+  
+
   return (
     <div>
       < Header />
       <h1>Informações da Viagem!</h1> 
-      { tripInfo && <><p>{tripInfo.name}</p>
+      { tripInfo &&
+      (tripInfo !== undefined ? ( 
+      <>
+      <p>{tripInfo.name}</p>
        <p>{tripInfo.planet}</p>
        <p>{tripInfo.durationInDays}</p>
        <p>{tripInfo.date}</p>
        <p>{tripInfo.description}</p>
+       <h2>Candidatos Pendentes</h2>
+       {pendingCanditates && pendingCanditates.length > 0 ? pendingCanditates : <h4> Não a candidatos pendentes</h4>}
+       <h2>Candidatos Aprovados</h2>
+       {aprovedCandidates && aprovedCandidates.length > 0 ?  aprovedCandidates : <h4>Não a candidatos aprovados </h4>}
        </> 
-      }
+      ) : (
+        <h1> Não Há Viagens planejadas! </h1>      
+        ))}
       </div>
   )
 }

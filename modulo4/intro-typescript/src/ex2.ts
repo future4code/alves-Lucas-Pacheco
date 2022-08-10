04867b3450fd2d04582bb71122c472e5441b7441
Lucas Magalhães Pacheco <lucasmagalhaespacheco@gmail.com>
2022-08-10 15:26:25 -0300

    Sem os desafios

---
+++ modulo4/intro-typescript/src/ex2.ts
@@ -0,0 +1,8 @@
+const imprimaTrêsCoresFavoritas = () :string[] => {
+const cor1: string = "Vermelho"
+const cor2: string = "Azul"
+const cor3: string = "Verde"
+return ([cor1, cor2, cor3])
+}
+
+console.log(imprimaTrêsCoresFavoritas())

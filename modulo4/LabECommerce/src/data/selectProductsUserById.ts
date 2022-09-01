02357485e68d67fd407fe0848434961e2081f95f
Lucas Magalhães Pacheco <lucasmagalhaespacheco@gmail.com>
2022-09-01 12:52:24 -0300

    arrumando ultimo

--- modulo4/LabECommerce/src/data/selectProductsUserById.ts
+++ modulo4/LabECommerce/src/data/selectProductsUserById.ts
@@ -3,8 +3,9 @@ import connection from "./connection";
 const selectProductsUserById = async (userId: string): Promise<any> => {
 
     const result =  await connection("labecommerce_purchases")
-        .select("*")
         .where("user_id", userId)
+        .join("labecommerce_products", "labecommerce_purchases.product_id", "labecommerce_products.id" )
+        .select("labecommerce_products.name", "product_id", "quantity", "total_price")
     
 
         return result
